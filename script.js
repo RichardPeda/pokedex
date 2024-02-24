43358e58cd8b0fb65e40d1aace9642aca80ca2ff
RichardPeda <richard.peda@gmx.net>
2024-02-24 19:45:20 +0100

    first render of small card

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-let limit = 3;
+let limit = 10;
 let offset = 0;
 
 
 let pokeURL = [];
 let pokeData = [];
-let Pokemoves = [];
-let moves = [];
-// let move = [];
-let myarray = [];
-let length = [];
+
+
+async function init() {
+
+    await loadPokemonURL();
+    await loadPokeDetails();
+    renderCard();
+}
+
+
+
+
 
 async function loadPokemonURL() {
     let url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
@@ -20,76 +27,56 @@ async function loadPokemonURL() {
         pokeURL.push(result['url']);
     });
 
-    loadPokeDetails()
+
 }
 
 
 async function loadPokeDetails() {
-    // pokeData.forEach(data => {
-    //     pokeURL.push(data['url'])
-    // })
 
     for (let i = 0; i < pokeURL.length; i++) {
         let url = pokeURL[i];
-        
+
         let response = await fetch(url);
         let responseAsJson = await response.json();
 
-pokeData.push(responseAsJson)
+        pokeData.push(responseAsJson)
 
-        console.log(responseAsJson)
-    // let results = responseAsJson['results'];
     }
-
-
-        // console.log(responseAsJson)
-        // moves.push(responseAsJson['moves'])
-
-        // pokeData[i].id = getID(responseAsJson)
-        // pokeData[i].types = getTypes(responseAsJson)
-        // pokeData[i].abilities = getAbilities(responseAsJson)
-        // pokeData[i].sprite = getSprites(responseAsJson)
-
-        // for (let j = 0; j < moves.length; j++) {
-        //     let move = moves[j];
-        //     myarray = [];
-        //     for (let k = 0; k < move.length; k++) {
-        //         let element = move[k]['move']['name'];
-        //         myarray.push(element)
-        //     }
-        //     pokeData[i].moves = (myarray);
-        // }
-    
+    console.log(pokeData)
 }
-// function getID(dataSet) {
-//     return dataSet['id']
-// }
 
-
-// function getTypes(dataSet) {
-//     let types = dataSet['types'];
-//     let array = []
-//     types.forEach(type => {
-//         array.push(type['type']['name'])
-//     });
-//     return array
-// }
-
-// function getAbilities(dataSet) {
-//     let abilities = dataSet['abilities'];
-//     let array = []
-//     abilities.forEach(ability => {
-//         array.push(ability['ability']['name'])
-//     });
-//     return array
-// }
-
-// function getSprites(dataSet, i) {
-//     let sprite = dataSet['sprites']['front_default']
-//     return sprite;
-// }
 
 function renderCard() {
     let container = document.getElementById('card-container');
-    container.innerHTML = ``
+    container.innerHTML = ''
+    for (let index = 0; index < pokeData.length; index++) {
+        container.innerHTML += smallCardHTML(index)
+
+    }
+
+
+}
+
+function smallCardHTML(index) {
+    let html = /*html*/`
+    <div class="card">
+                <div class="card-inner">
+                    <div class="card-header">
+                        <h2 id="name">${pokeData[index]['name']}</h2>
+                        <div id="card-id">ID 000${pokeData[index]['order']}</div>
+                    </div>
+                    <div class="ability-container">
+                        <div class="ability primary">${pokeData[index]['types'][0]['type']['name']}</div>
+                        <div class="ability secondary">${pokeData[index]['types'][1]['type']['name']}</div>
+                    </div>
+                    <div class="image-container">
+                        <img src="${pokeData[index]['sprites']['other']['home']['front_default']}"
+                            alt="">
+                    </div>
+                </div>
+               <div class="platform"></div>
+
+            </div>
+`
+    return html
 }
